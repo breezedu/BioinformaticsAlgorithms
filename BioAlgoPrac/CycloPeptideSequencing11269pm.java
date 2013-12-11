0c5ff4c79879ce40415961117d416216e57d2937
breezedu <breeze.du@gmail.com>
2013-12-11 13:12:31 -0500

    transfer txt documents to a folder

---
+++ BioAlgoPrac/CycloPeptideSequencing11269pm.java
@@ -0,0 +1,344 @@
+package assignments;
+
+import java.util.Scanner;
+
+
+/*******************************************************
+ * Input a group of Mass Spectrum peaks; store them into an array;
+ * 1st compare with the dictionary; figure out how many proteins are there; 
+ * 2nd build sequence array; 
+ * only works when the sequence is less than 10 proteins;
+ * @author Frog
+ *
+ */
+
+public class CycloPeptideSequencing11269pm {
+	
+	public static void main(String[] args){
+		CycloPeptide.run(112);
+	} // end main();
+	
+	static CycloPeptideSequencing11269pm CycloPeptide = new CycloPeptideSequencing11269pm();
+	// with the static CycloPeptide main object, we can use private classes; 
+	
+	private class proteinUnit{
+		char name;
+		int molWeight;
+	}
+
+	private class ProteinDic{
+		char name;
+		int MolWeight;
+	}
+	
+	/******
+	private class cycloPeptide {
+		char name;
+		int Mass;
+		int Next;
+	}
+	*/
+	
+	private void run(int MM){
+		
+		// System.out.println("How many Integers are there in the document?");
+		//int MM = 92;
+		int numOfPep = (int) Math.sqrt(MM-2) +1; // num of peptes are n*(n-1), here we got MM = (n-1) first;
+		// Here we know there are 10 peptide unites in the cycloPeptide; 
+
+		int[] Peaks = new int[MM]; 
+		Scanner sc = new Scanner(System.in); 
+		System.out.print("Enter "+MM+" numbers on one line with spaces: \n"); 
+		
+		sc.useDelimiter(" "); 
+		for(int i = 0; i<Peaks.length; i++){ 
+		Peaks[i] = sc.nextInt(); 
+		} 
+		
+		sc.close();
+		
+		System.out.println("There are " + numOfPep + " peptides on the cyclopeptide ring.");
+		for(int i=0; i<MM; i++){
+			System.out.print(" " + Peaks[i]);
+			if (i% (numOfPep-1) == 0)
+				System.out.println();
+		} // end of for i loop;
+		
+		System.out.println("\n OK, 1st part.\n");
+		
+		
+		/***********************
+		 * build the dictionary of proteins and their molecular weight;
+		 */
+		
+		ProteinDic[] Proteins = buildProteinDictionary();
+		
+		/*******************************
+		 * build a new set of proteinUnit[] objects, 
+		 * to store the MolWeight of each peptide on the cycloPeptide;
+		 * cycloPeptide[i].right and cycloPeptide[i].left both pointing to itself; 
+		 */
+		proteinUnit[] proUnits = new proteinUnit[numOfPep];
+		int numUnits = 0;
+		for(int i=0; i< MM; i++){
+			for(int j=0; j<20; j++){
+				if (Proteins[j].MolWeight == Peaks[i]){
+					
+					// System.out.print("One Match: " + Peaks[i] + ". numUnits=" +numUnits +". " );
+					proUnits[numUnits] = CycloPeptide.new proteinUnit();
+					proUnits[numUnits].name = Proteins[j].name;
+					proUnits[numUnits].molWeight = Proteins[j].MolWeight;
+					numUnits++;
+					break; // this break is very important, to stop duplicated overwrite; 
+					
+				} // end if Proteins[].MolWeight == Peaks[];
+			} // end for j<20 loop;
+		} // end i < MM loop
+		 
+		/*******
+		 * try to print out all proUnits information
+		 * see below code;
+		 */
+		
+		for(int i=0; i<numOfPep; i++){
+			System.out.println("proUnits[" + i +"].name=" + proUnits[i].name + ". MolWeight=" + proUnits[i].molWeight);
+		}
+		
+				
+		System.out.println("All protein units built. The second part is OK.\n");
+
+		/************
+		 * initial an array of strings from StrSequences[0] to StrSequences[9];
+		 * StrSequences[0] will store all protein sequences in the mass spectrum with only 1 protein unit;
+		 * StrSequences[1] will store all protein sequences in the mass spectrum with two protein units;
+		 * etc, etc, etc, etc, etc;
+		 * StrSequences[9] will store one chain of numOfPep protein sequence with all protein unites in the spectrum;
+		 */
+		String[] StrSequences = new String[numOfPep];  // build numOfPep string[] arrays.
+		for(int i=0; i<numOfPep; i++){
+			
+			StrSequences[i] = "";
+		} // end for i loop; at the beginning, assign every string[] array "", empty. 
+		
+		
+		for(int i=0; i<numOfPep; i++){
+			StrSequences[0] += proUnits[i].name + "*";
+		}
+		//	StrSequences[0] += cycloPeptide[0].name +"*"; // make the StrSequences[0] = cycloPeptide[0].name;
+		
+		System.out.println(" StrSequences[0] = " + StrSequences[0]);
+		
+		int Len = 1;
+		while(Len<numOfPep){
+			
+			//System.out.print("(Length of the sequence) Len= " + Len); // everything is ok till Len = 8;
+			//System.out.println(" Strseq[Len-1]= " + StrSequences[Len-1]);
+			
+			for(int stringIndex=0; stringIndex<StrSequences[Len-1].length(); stringIndex += (Len+1)){
+				String subStr = StrSequences[Len-1].substring(stringIndex, stringIndex+Len);
+			//	System.out.println("subStr= " + subStr +". ");
+
+				for(int i=0; i<numOfPep; i++){
+					
+					String seqStr =subStr + proUnits[i].name; // make the first part of seqStr = subStr (the old sequence of Len-1);
+
+					int Weight = getWeight(seqStr, Proteins);
+					//System.out.print(" Weight= " + Weight +" seqStr=" + seqStr +";  ");
+					
+					for(int j=0; j<MM; j++){
+						if (Peaks[j] == Weight && !StrSequences[Len].contains(seqStr)){	
+							// Only when there's no duplicate string in the StrSequences[Len], will we save the seqStr into StrSequences[Len];
+							if( checkValid(seqStr, Peaks, MM, proUnits, Proteins))
+								StrSequences[Len] += (seqStr + "*"); 
+								
+							} // end of if condition; 
+							
+						} // end of for j loop;
+					
+				} // end of for i loop;
+											
+			} // end of for strings in the strSequences[Len-1];
+			System.out.println(" StrSequences["+Len+"] = " + StrSequences[Len]);
+			Len ++;
+		} // end of while;
+		System.out.println("After while loop Len=" + Len +". \nThe third part is OK!\n");
+		
+		//System.out.println("\nStrSequences["+(numOfPep-1)+"]=" + StrSequences[numOfPep-1] +". "
+		//					+ "the last step is the check whether each sequence makes sense;");
+		
+		/***********
+		 * check each sub sequence at the last StrSequences[9].
+		 * To see which one meet all the Mass spectrum requirment;
+		 * Final String: PHAKPAIIVHA. 
+		 * Mass: 97-137-71-128-97-71-113-113-99-137-71 total weight is: 1134
+		 * Final String: PHAKPSPIVHA. 
+		 * Mass: 97-137-71-128-97-87-97-113-99-137-71 total weight is: 1134
+		 * Sometimes, the coinsicent just happens, like the two subStrings upperl
+		 * they have exactly the same MolWeight, and they both meet the validCheck() method;
+		 * So here we have to add a final check step for the final strings:
+		 * to see if every cycloPeptide is here in the final strings;
+		 */
+		for(int i=0; i<StrSequences[numOfPep-1].length(); i+=(numOfPep+1)){
+			String subStr = StrSequences[numOfPep-1].substring(i, i+numOfPep);
+			boolean lastCheck = false;
+			lastCheck = finalCheck(subStr, proUnits);
+			if(lastCheck){
+				System.out.print("\nFinal String: " + subStr +". Mass: ");
+				transSeqtoMass(subStr, Proteins);
+			} // end if(fCheck);
+			
+		} // end for i<StrSequence[numOfPep-1].length loop; 	
+		
+		System.out.println("\nFinal part ready! Game Over!");
+
+	}// end of run();
+	
+	private static boolean finalCheck(String subStr, proteinUnit[] proUnits) {
+		// TO see if every cycloPeptide[i] is in the sequence;
+		int Leng = proUnits.length;
+		int strLen = subStr.length();
+		int count =0;
+		
+		for(int i=0; i<Leng; i++){
+			
+			for(int j=0; j<strLen; j++){
+				
+				if(subStr.charAt(j) == proUnits[i].name){
+					count++;
+					break;
+				}
+			} // end for (j < strLen) loop;
+		} // end for (i<Leng) loop;
+		
+		// System.out.println("count=" + count + " Leng=" + Leng + " subStr.Length=" + strLen);
+		if(count < Leng) {
+			return false;
+		} else return true;
+		
+	} // enf finalCheck() method;
+
+	private static void transSeqtoMass(String subStr, ProteinDic[] Proteins) {
+		// TO transfer a sequence of amino acids into a Mass sequence;
+		int Length = subStr.length();
+		for(int i=0; i<Length-1; i++){
+			for(int j=0; j<20; j++){
+				if(subStr.charAt(i) == Proteins[j].name){
+					System.out.print(Proteins[j].MolWeight + "-");
+					break;
+				}
+				
+			} // end for j<20 loop
+		} // end for i<Length loop
+		for(int j=0; j<20; j++){
+			if(subStr.charAt(Length-1) == Proteins[j].name){
+				System.out.print(Proteins[j].MolWeight +" ");
+			}
+		}
+		
+	} // end transSeqtoMass() method;
+
+	private static ProteinDic[] buildProteinDictionary() {
+        // TO build the protein dictionary; with protein ID and related MolWeight;
+		/*******
+		 * As we know there are 20 natural amino acids;
+		 * so we build an object array ProteinDic[20], each represents an amino acid;
+		 * input all name and molecular weight for each ProteinDic[i];
+		 * finish the dictionary;
+		 */
+		
+        ProteinDic[] ProteinID = new ProteinDic[20];
+        for(int i=0; i<20; i++){
+        	/********
+        	 * the ProteinDic() class inherits from CycloPeptideSequence1122{}
+        	 * the ProteinDic1() is a separated class;
+        	 */
+            ProteinID[i] = CycloPeptide.new ProteinDic(); 
+        }   
+        ProteinID[0].name = 'G';        ProteinID[0].MolWeight = 57; 
+        ProteinID[1].name = 'A';        ProteinID[1].MolWeight = 71;     
+        ProteinID[2].name = 'S';        ProteinID[2].MolWeight = 87;     
+        ProteinID[3].name = 'P';        ProteinID[3].MolWeight = 97;
+        ProteinID[4].name = 'V';        ProteinID[4].MolWeight = 99;
+        ProteinID[5].name = 'T';        ProteinID[5].MolWeight = 101;
+        ProteinID[6].name = 'C';        ProteinID[6].MolWeight = 103;
+        ProteinID[7].name = 'I';        ProteinID[7].MolWeight = 113;
+        ProteinID[8].name = 'L';        ProteinID[8].MolWeight = 113;
+        ProteinID[9].name = 'N';        ProteinID[9].MolWeight = 114;
+        ProteinID[10].name = 'D';       ProteinID[10].MolWeight = 115;
+        ProteinID[11].name = 'K';       ProteinID[11].MolWeight = 128;
+        ProteinID[12].name = 'Q';       ProteinID[12].MolWeight = 128;
+        ProteinID[13].name = 'E';       ProteinID[13].MolWeight = 129;
+        ProteinID[14].name = 'M';       ProteinID[14].MolWeight = 131;
+        ProteinID[15].name = 'H';       ProteinID[15].MolWeight = 137;
+        ProteinID[16].name = 'F';       ProteinID[16].MolWeight = 147;
+        ProteinID[17].name = 'R';       ProteinID[17].MolWeight = 156;
+        ProteinID[18].name = 'Y';       ProteinID[18].MolWeight = 163;
+        ProteinID[19].name = 'W';       ProteinID[19].MolWeight = 186;
+        
+        return ProteinID;
+        
+ } // end private static ProteinDic[];
+
+	private static boolean checkValid(String testStr, int[] Peaks, int MM, proteinUnit[] proUnits, ProteinDic[] Proteins) {
+		/**************
+		 * 1st, get the length of the string to test; assign it to Length;
+		 * 2nd, get subStrings, from the Length-1 till 2, we do not need to check 1 unit and the whole unit;
+		 * 3rd, for each length of the subString, there might be (Length-subStr.length +1) different subStrings;
+		 *      Ex: for a 10-unit testString, there might be 2 9-unit substrings; 3 8-unit substrings;
+		 *      untill 9 2-unit substrings;
+		 */		
+		
+		int Length = testStr.length();
+		// after we get the length of the string; we should test all subSequences from that testStr;
+
+		int subLen = Length-1;
+		// boolean check=false;
+		
+		while(subLen>1){
+			int count=0; // each subString should have a match from the mass spectrum;
+			
+			for(int i=0; i<Length-subLen; i++){
+				String tempStr = testStr.substring(i, i+subLen);
+				int tempWeight = getWeight(tempStr, Proteins);
+				for(int j=0; j<MM; j++){
+					if(tempWeight == Peaks[j])
+						count++;
+				} // end for (j<MM) loop;
+				
+			}// end for(i<Length-subLen) loop;
+			if (count < (Length-subLen)){
+				return false; // at any loop, 
+							//if the subString does not match any Peak in the spectrum, return False;
+				
+			}
+			
+			subLen--;
+		} // end while (subLen>1); 
+		
+		return true;
+	} // end of checkValid() method;
+
+	private static int getWeight(String subStr, ProteinDic[] proteins) {
+		// TO get the molecular weight of a certain protein sequences;
+		/******************
+		 * 1st, get each protein on the sequence;
+		 * 2nd, compare to the protein-dictionary, to get the molecular weight of each unit;
+		 * 3rd, calculate and sum up all protein unit's MolWeight;
+		 * 4th, return that sum.
+		 */
+		int MolWeight=0;
+		int Length = subStr.length();
+		for(int i=0; i<Length; i++){
+			// use ProteinUnite to get each protein on the sequence;
+			char ProteinUnite = subStr.charAt(i);
+			for(int j=0; j<20; j++){
+				//compare the protein to the protein-dictionary; to get the molecular weight of that protein unite;
+				if(proteins[j].name == ProteinUnite)
+					MolWeight += proteins[j].MolWeight;
+			} // end for j<20 loop;
+		} // end for i<subStr.length() loop;
+		
+		return MolWeight;
+	} // end getWeight() method;
+
+} // :) end of the whole class;
---
+++ BioAlgoPrac/FreWordMiss3.java
@@ -0,0 +1,259 @@
+package assignments;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FreWordMiss3 {
+	
+	public static void main(String[] args) throws IOException{
+		
+		@SuppressWarnings("resource")
+		//String OriStr = new Scanner(new File("Thermotoga-petrophila.txt")).useDelimiter("\\A").next();
+		String OriStr = new Scanner(new File("dataset_8_45.txt")).useDelimiter("\\A").next();
+		
+		OriStr += "MN";
+		/***************************************************************************************
+		 *  Add another 2 chars 'MN' to the end of the OriStr, as the end point of the String.
+		 *  after adding a char different from AGTC, the array.length would be 2 ele-longer than string.length();
+		 *  the last two sub-sequences would never be the same to any sub-sequence ahead, thus the index.value of
+		 *  the last two sub-sequences would never change.
+		 *  even during recursion, the very last sub-sequence would never change. 
+		 *  to ensure in the following count and comparing progress, there won't be over flow.
+		 */
+		
+		// System.out.println(OriStr);
+		int oriLeng = OriStr.length()-2;
+		
+		/***************************************************************************************
+		 * input length of sequence to be checked; 
+		 * normally the 9-element subsequence would be the k-mer
+		 * for the ribosome to check and bind to, :) HERE we just input the number directly;
+		 * In real program we have to use arg[0], or write a method to ask user to input the subSeqLen.
+		 */
+		int subSeqLen = 10;
+		
+		String NoMissStr="";
+		
+		NoMissStr = frequentCompare(OriStr, subSeqLen, oriLeng, NoMissStr);
+		
+		missMatch(OriStr, NoMissStr, subSeqLen);
+		
+	} // end main(); ATGCTGAGT
+
+	private static void missMatch(String oriStr, String missStr, int subSeqLen) {
+		System.out.println("Begin OneMissMatch");
+		
+		// TO check 1-miss Most Frequent Str, by replacing each element of the missStr to be ATGC
+		
+		System.out.println("New String is: " + missStr +" \n");
+		int numOfStr = missStr.length()/subSeqLen;
+		String[] newStrings = new String[numOfStr];
+		
+		System.out.println("There should be " + (numOfStr) + " sub strings in the new string.");
+		for (int i=0; i<numOfStr; i++){
+			newStrings[i] = missStr.substring(i*10, i*10 +10); 
+			System.out.print( newStrings[i] +" ");
+		}
+		
+		char[] mid = new char[4];
+		mid[0] = 'A';
+		mid[1] = 'G';
+		mid[2] = 'T';
+		mid[3] = 'C';
+		
+		String miss1String = "";
+		
+		for (int i=0; i<numOfStr; i++){
+			for (int j=0; j<4; j++){
+				for(int k=0; k<subSeqLen; k++){
+					String miss1Str = newStrings[i].substring(0, k) + mid[j] +newStrings[i].substring(k+1);					
+					
+					miss1String = miss1MatchCount(miss1Str, oriStr, miss1String);
+					
+				} // enf for k loop;
+				
+			} // end for j loop;
+		} // end for i loop;
+		
+		System.out.println("\n New 1miss String: " + miss1String);
+
+		System.out.println("Well done the One Miss Match()");
+	}
+
+	private static String miss1MatchCount(String miss1Str, String oriStr, String miss1String) {
+		// TO check if the miss1Str matches 1-char-miss in the oriStr;
+		int N = miss1Str.length();
+		int M = oriStr.length();
+		
+		for(int i=0; i<M-N; i++){
+			String tempStr = oriStr.substring(i, i+N);
+			int match =0;
+			
+			for(int k=0; k<N; k++){
+				if(tempStr.charAt(k) == miss1Str.charAt(k))
+					match++;
+			}
+			
+			if (match >= N-2)
+				miss1String += miss1Str;
+		}
+		
+		return miss1Str;
+		
+	}
+
+	private static String frequentCompare(String oriStr, int subLength, int oriLength, String missStr) throws IOException {
+		// TO check how many times a subsequence in the DNA sequence repeated
+		// to begin with the very first 9 (length) lone-base-pair as the begin sequence
+		// check all subsequence in the whole sequence and compare it the the begin sequence
+		
+		int M = oriLength;
+		int N = subLength;
+		int Max = 0;
+		int [] seqIndex = new int[oriLength];
+		
+		for(int i=0; i<oriLength; i++){
+			seqIndex[i] = i;
+		}
+		
+		// Max will store the time of most frequently sequence repeated.
+		int count =0;
+		int paternNumber=0;
+		// count to store the time of each subsequence repeated comparing to the sequence emerge after it.
+		// so there might be some repeated output sequence if I want to output all subsequence
+		// that repeated over 100 times.
+		
+		System.out.println("The length of input string is: " + (N));
+		
+		// print out the total lone-base-pair in the original sequence.
+		
+		File f=new File("dataset_4_45res.txt");
+		FileWriter out=new FileWriter(f);
+		
+		// use the fileWriter to output all subsequence repeated over 100 times.
+			int index =0;
+			
+		while(index < M-N){
+			
+			int i = newRoot(seqIndex, index);
+
+			String strTemp = oriStr.substring(i, i+N);
+									
+			/****************************************************************************
+			 * after we confirm the startpoing of temStr at i;
+			 * we should compare all N-length sequence after i till the end of OriStr.
+			 * Meanwhile, all the N-length subsequence begins at charAt(i+1), and ofcourse we
+			 * have to avoid all sequence which has already been compared.
+			 * so every time j = newRoot(seqIndex(j+1)); instead of j++;
+			 */
+			int j = newRoot(seqIndex, i+1);
+			while (j<(M-N)){
+				int startPoint = j;
+				int endPoint = startPoint+N;
+				
+				String comStr = oriStr.substring(startPoint, endPoint);
+				
+				if (strTemp.equals(comStr)){
+					
+					/*******************************
+					 *  System.out.print("Change seqIndex["+j+"]'s value to ");
+					 *  Check the final root of seqIndex[j];
+					 *  then pass the value to seqIndex[j];
+					 *  next time this start point will be ignored;
+					 */
+					
+					
+					seqIndex[startPoint]=newRoot(seqIndex, startPoint+1);
+
+					count++;
+				}
+				
+				
+				/************************************************************ 
+				 * pass new value, the final root of seqIndex[j+1] to a new j;
+				 * then repeat the string compare.
+				 */
+				j = newRoot(seqIndex, j+1);
+				
+			}
+			
+			// System.out.println("\n Running at " + index + " point. the real index is " + i );
+			
+			/***********************************************************************************
+			 * previous run test showed some 9-element sub sequences repeated around 120 times
+			 * so we set monitor point at 110, if a sub-sequence repeated over 110 time
+			 * print out the sub-sequence and the time it repeated in the whole string.
+			 */
+			
+			if (count >= 1){
+				paternNumber++;
+				missStr += strTemp;
+				// System.out.println("missStr: " + missStr);
+				
+				System.out.println(paternNumber +" At charAt " + index + " root " + i + " the sequence " + strTemp + " repeated " + (count+1) +" times.");
+				
+				out.write(count + ": " + strTemp + ". ");
+			}
+			
+			
+			/**********************************************************************************
+			 * store the new count value to Max if we got a new value bigger than 
+			 * the Max we got last time.
+			 * we can put the out.write() method here to ensure the last few sequences are the 
+			 * most frequently repeat sub-sequences.
+			 * here in this program, all sub-sequences repeated over 110 time will be written to 
+			 * document ResultThermotoga34th.txt
+			 */
+			if (count >= Max){
+				Max = count;
+			}
+			
+			count=0;
+			
+			/**********************************************************************************
+			 * make i+1 the next index to count.
+			 * every time pass i+1 to newRoot() method, 
+			 * figure out the final root then generate new sub-sequence 
+			 * begin sequence compare
+			 */
+			index = i+1;
+			
+		}// end outer while loop;
+		
+		/***********************************************************************************
+		 * Print out the Max, the repeated time of most frequent sequence.
+		 * then close the FileWriter OUT.
+		 */
+	       System.out.println("\n Max = " + Max + " :) Done ..........");
+	       out.close();
+		return missStr;
+	} // end frequent compare method;
+
+	
+	
+	private static int newRoot(int[] seqIndex, int j) {
+		// TO find out the final root of seqIndex[j];
+		
+		while(j != seqIndex[j]){
+			j =seqIndex[j];
+		} // the recursion!
+
+		return j;
+	} // end of newRoot method.
+
+}
+
+/**************************************************************************************
+what you want to generate is a set of strings that contains the base k-mer, 
+plus all strings with 1-mismatch, plus all strings with 2-mismatches, and so on
+ up to d-mismatches.  to get the set of 1-mismatches, you take the base k-mer 
+ and for each character of it you generate a new string by replacing that 
+ character with each character of 'ACGT'.  add all these generated strings 
+ to your set.  for 2-mismatches, we realize that we can take the 1-mismatch
+  strings and generate 1-mismatches from all those.  hope this explanation helps.  
+  there's a lot of steps to figure out and it's a series of several loops to do this,
+   but start with smaller steps by breaking the problem down, that should help get 
+   you to working code.
+****************/
